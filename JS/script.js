7b62d5a241d49caace30c2cd7b3032fdf65e9bd2
Skylark355 <adzesi.chris@gmail.com>
2024-12-05 12:17:09 +0000

    Done styling

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// hamburger code starts here
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
@@ -13,4 +14,36 @@ navItems.forEach((item) => {
   item.addEventListener("click", () => {
     hideSidebar(); // Hide the sidebar on click
   });
+});
+
+// hamburger codes ends here
+
+// new code
+// document.querySelectorAll(".review-slider img").forEach((images) => {
+//   images.onclick = () => {
+//     var src = images.getAttribute("src");
+//     document.querySelector(".main-home-image").src = src;
+//   };
+// });
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
 });
